23ff5c444fa4b0e6e61b1c3f99542dc0145699de
nourhantaha21 <norhantaha43@gmail.com>
2025-07-06 17:41:57 +0300

    Add files via upload

---
+++ Search/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double percent = 0.85;
+            Console.WriteLine($"Success Rate: {percent:P}");
+        }
+    }
+}
